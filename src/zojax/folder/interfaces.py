c6f25124e1835efac6c5d9613887e96767bf1e7c
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2009-12-24 15:16:01 +0000

    upload the fork of z3ext

---
+++ src/zojax/folder/interfaces.py
@@ -0,0 +1,27 @@
+##############################################################################
+#
+# Copyright (c) 2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+""" zojax.folder interfaces
+
+$Id$
+"""
+from zope import interface
+from zojax.content.type.interfaces import IItem
+
+
+class IFolder(IItem):
+    """ Folder """
+
+
+class IFolderType(interface.Interface):
+    """ folder content type """
